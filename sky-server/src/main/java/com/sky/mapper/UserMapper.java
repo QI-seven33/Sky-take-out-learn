57c24a86626832fda216e5d04fb5c3407c8e286d
sevan <2595464764qq@.com>
2026-06-20 23:32:07 +0800

    首次提交项目（已移除敏感信息）

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据openid来查询用户
+     * @param openId
+     * @return
+     */
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenId(String openId);
+
+    /**
+     * 插入数据
+     * @param user
+     */
+    void insert(User user);
+
+
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
+
+    /**
+     * 根据动态条件拓展
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
+}
